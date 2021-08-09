8fdfdcab244fe91b11535388d855bf7549890a0a
Elijah Wilcott <ejw773@gmail.com>
2021-08-09 17:25:34 -0400

    adds future data to projectInfo

--- src/shared/projectInfo.js
+++ src/shared/projectInfo.js
@@ -6,9 +6,30 @@ const projectInfo = [
     },
     {
         day: 2,
-        title: 'This one has not happened yet',
+        title: 'TBD',
         link: 'https://todo02.elijahwilcott.com/'
+    },
+    {
+        day: 3,
+        title: 'TBD',
+        link: 'https://todo03.elijahwilcott.com/'
+    },
+    {
+        day: 4,
+        title: 'TBD',
+        link: 'https://todo04.elijahwilcott.com/'
+    },
+    {
+        day: 5,
+        title: 'TBD',
+        link: 'https://todo05.elijahwilcott.com/'
+    },
+    {
+        day: 6,
+        title: 'TBD',
+        link: 'https://todo06.elijahwilcott.com/'
     }
+
 ];
 
 export default projectInfo;
