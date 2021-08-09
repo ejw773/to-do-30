5653d1232e7d2c5c84bf7697bea4daa7967d52b6
Elijah Wilcott <ejw773@gmail.com>
2021-08-09 19:32:31 -0400

    modifies package.json, removes unneccessary import

--- src/components/CardContainerComponent.js
+++ src/components/CardContainerComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
 import ProjectCard from './CardComponent'
 
 const CardContainer = () => {
--- src/components/JumbotronComponent.js
+++ src/components/JumbotronComponent.js
@@ -4,7 +4,7 @@ const Jumbotron = () => {
     return (
         <div className="jumbotron">
             <h1>To-Do 30</h1><br />
-            <p><a href="https://github.com/ejw773/to-do-30" target="_blank">"To-Do 30"</a> is a challenge to create 30 different to-do apps, each within a one-day period.</p>
+            <p><a href="https://github.com/ejw773/to-do-30" target="_blank" rel="noreferrer">"To-Do 30"</a> is a challenge to create 30 different to-do apps, each within a one-day period.</p>
             <p>My collection of to-do apps is below.</p>
         </div>
     )
