012f7e38418fbb721314e2270e5c836a5b4d8d62
Elijah Wilcott <ejw773@gmail.com>
2021-08-09 18:56:03 -0400

    updates shared projectInfo file

--- src/shared/projectInfo.js
+++ src/shared/projectInfo.js
@@ -28,8 +28,127 @@ const projectInfo = [
         day: 6,
         title: 'TBD',
         link: 'https://todo06.elijahwilcott.com/'
+    },
+    {
+        day: 7,
+        title: 'TBD',
+        link: 'https://todo07.elijahwilcott.com/'
+    },
+    {
+        day: 8,
+        title: 'TBD',
+        link: 'https://todo08.elijahwilcott.com/'
+    },
+    {
+        day: 9,
+        title: 'TBD',
+        link: 'https://todo09.elijahwilcott.com/'
+    },
+    {
+        day: 10,
+        title: 'TBD',
+        link: 'https://todo10.elijahwilcott.com/'
+    },
+    {
+        day: 11,
+        title: 'TBD',
+        link: 'https://todo11.elijahwilcott.com/'
+    },
+    {
+        day: 12,
+        title: 'TBD',
+        link: 'https://todo12.elijahwilcott.com/'
+    },
+    {
+        day: 13,
+        title: 'TBD',
+        link: 'https://todo13.elijahwilcott.com/'
+    },
+    {
+        day: 14,
+        title: 'TBD',
+        link: 'https://todo014.elijahwilcott.com/'
+    },
+    {
+        day: 15,
+        title: 'TBD',
+        link: 'https://todo15.elijahwilcott.com/'
+    },
+    {
+        day: 16,
+        title: 'TBD',
+        link: 'https://todo16.elijahwilcott.com/'
+    },
+    {
+        day: 17,
+        title: 'TBD',
+        link: 'https://todo17.elijahwilcott.com/'
+    },
+    {
+        day: 18,
+        title: 'TBD',
+        link: 'https://todo18.elijahwilcott.com/'
+    },
+    {
+        day: 19,
+        title: 'TBD',
+        link: 'https://todo19.elijahwilcott.com/'
+    },
+    {
+        day: 20,
+        title: 'TBD',
+        link: 'https://todo20.elijahwilcott.com/'
+    },
+    {
+        day: 21,
+        title: 'TBD',
+        link: 'https://todo21.elijahwilcott.com/'
+    },
+    {
+        day: 22,
+        title: 'TBD',
+        link: 'https://todo22.elijahwilcott.com/'
+    },
+    {
+        day: 23,
+        title: 'TBD',
+        link: 'https://todo23.elijahwilcott.com/'
+    },
+    {
+        day: 24,
+        title: 'TBD',
+        link: 'https://todo024.elijahwilcott.com/'
+    },
+    {
+        day: 25,
+        title: 'TBD',
+        link: 'https://todo25.elijahwilcott.com/'
+    },
+    {
+        day: 26,
+        title: 'TBD',
+        link: 'https://todo26.elijahwilcott.com/'
+    },
+    {
+        day: 27,
+        title: 'TBD',
+        link: 'https://todo27.elijahwilcott.com/'
+    },
+    {
+        day: 28,
+        title: 'TBD',
+        link: 'https://todo28.elijahwilcott.com/'
+    },
+    {
+        day: 29,
+        title: 'TBD',
+        link: 'https://todo29.elijahwilcott.com/'
+    },
+    {
+        day: 30,
+        title: 'TBD',
+        link: 'https://todo30.elijahwilcott.com/'
     }
-
 ];
 
 export default projectInfo;
