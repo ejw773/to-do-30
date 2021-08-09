61c79f8041c1514c6e7345349a006ce52e118d34
Elijah Wilcott <ejw773@gmail.com>
2021-08-09 16:40:43 -0400

    removes unneccessary imports

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 const ProjectCard = ({item}) => {
     return (
--- src/components/CardContainerComponent.js
+++ src/components/CardContainerComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import projects from '../shared/projectInfo';
 import ProjectCard from './CardComponent';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -8,7 +7,11 @@ import Col from 'react-bootstrap/Col'
 const CardContainer = () => {
     return (
         <Container>
-            
+            <Row>
+                <Col>
+                    <p>Card Container</p>
+                </Col>
+            </Row>
         </Container>
     )
 }
