7eb20037aabc774cf403f07ccd4f0ae71d805b0a
Elijah Wilcott <ejw773@gmail.com>
2021-08-10 11:12:39 -0400

    removes unnecessary <a> tag

--- src/components/ListContainerComponent.js
+++ src/components/ListContainerComponent.js
@@ -3,9 +3,9 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 const ListContainer = ({tasks, toggleStatus}) => {
     const taskList = tasks.map((item) => 
-        <a onClick={toggleStatus}><ListGroup.Item key={item.id}>
+        <ListGroup.Item key={item.id}>
             {item.task}
-        </ListGroup.Item></a>);
+        </ListGroup.Item>);
     return (
         <div className='m-3'>
             <ListGroup>
