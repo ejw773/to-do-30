b6cef5a191ee8fb86f7508d1fc1c14ca72c16f61
Elijah Wilcott <ejw773@gmail.com>
2021-08-11 14:15:04 -0400

    delete task now working

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,41 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './HeaderComponent';
 import NewTask from './NewTaskComponent';
 import TaskList from './TaskListComponent';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userInput: 'input',
-            taskList: [
-                {
-                    task: 'wash the dog',
-                    id: '0'
-                },
-                {
-                    task: 'watch a movie',
-                    id: '1'
-                }
-            ]
-        };
-    };
-    render() {
-        const handleChange = (newInput) => {
-            console.log('changed');
+const Main = () => {
+    // Set up State
+    const [taskList, setTaskList] = useState([
+        {
+            task: 'wash the cat',
+            id: '0',
+            isCompleted: false
+        },
+        {
+            task: 'watch a movie',
+            id: '1',
+            isCompleted: true
+        },
+        {
+            task: 'finish to-do app',
+            id: '2',
+            isCompleted: false
         }
-        const handleClick = () => {
-            console.log('clicked')
-        }
+    ]);
 
-        return (
-            <div>
-                <Header />
-                <NewTask userInput={this.state.userInput} handleClick={handleClick} handleChange={handleChange}/>
-                <TaskList taskList={this.state.taskList}/>
-            </div>
-        );
+    // Set up Methods
+    const handleDelete = ({target}) => {
+        alert(`Delete Task ${target.id}: ${target.value}?`);
+        let newTaskList = [...taskList];
+        newTaskList = taskList.filter((item) => item.id !== target.id);
+        setTaskList(newTaskList);
+    };
+    const toggleCompleted = ({target}) => {
+        let newTaskList = [...taskList];
+        newTaskList[target.id].isCompleted = !newTaskList[target.id].isCompleted;
+        setTaskList(newTaskList);        
     }
+    const createNewTask = () => {
+        console.log('create new task')
+    }
+
+
+    // Return JSX
+    return (
+        <div>
+            <Header />
+            {/* <NewTask /> */}
+            <TaskList taskList={taskList} handleDelete={handleDelete} toggleCompleted={toggleCompleted} />
+        </div>
+        
+    )
 }
 
 export default Main;
--- src/components/TaskListComponent.js
+++ src/components/TaskListComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const TaskList = ({taskList}) => {
+const TaskList = (props) => {
+    const { taskList, toggleCompleted, handleDelete } = props;
     return (
         <ul>
-            {taskList.map((item) => {return (
-                <div>
-                    <li key={item.id}>{item.task}</li>
-                    <button>Delete</button>
+            {taskList.map((item, index) => {return (
+                <div key={index}>
+                    <input type='checkbox' id={item.id} name={item.task} value={item.task} checked={item.isCompleted} onChange={toggleCompleted} />
+                    <label htmlFor={item.id}>{item.task}</label>
+                    <button id={item.id} onClick={handleDelete} value={item.task}>Delete</button>
                 </div>
             )})}
         </ul>
