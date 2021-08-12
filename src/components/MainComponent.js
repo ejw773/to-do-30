55e5eb1dc23f080bbb46acb7a66c041c52565c87
Elijah Wilcott <ejw773@gmail.com>
2021-08-12 06:31:44 -0400

    adds basic components

---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './HeaderComponent'
+import NewTask from './NewTaskComponent'
+import TaskList from './TaskListComponent'
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <NewTask />
+            <TaskList />
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/components/TaskListComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TaskList = () => {
+    return (
+        <div>
+            <h1>Task List</h1>
+        </div>
+    )
+}
+
+export default TaskList;
