9492a48ea4d50dd30a850914c52d885cf1af6409
Elijah Wilcott <ejw773@gmail.com>
2021-08-11 20:44:34 -0400

    adjusts header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="jumbotron text-center">
             <h1>To-Do 30: Day 3</h1>
-            <h2>Delete Tasks</h2>
+            <h2>Complete / Delete Tasks</h2>
         </div>
     )
 }
