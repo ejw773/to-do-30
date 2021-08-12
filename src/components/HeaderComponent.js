4200047fba8af0f64924d5d99b495786f0f0be30
Elijah Wilcott <ejw773@gmail.com>
2021-08-12 16:37:13 -0400

    adds portfoio to navbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,13 +6,14 @@ import { Nav } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <Navbar bg='light' expand='lg'>
+        <Navbar bg='dark' variant='dark' expand='lg'>
         <Container>
             <Navbar.Brand>To Do 30</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="https://github.com/ejw773/to-do-30" target="_blank">About</Nav.Link>
+                    <Nav.Link href="https://elijahwilcott.com/" target="_blank">Portfolio</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
