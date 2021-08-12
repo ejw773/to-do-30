3ccd105d141a6ef3009669b407c1c8d926a9a3a6
Elijah Wilcott <ejw773@gmail.com>
2021-08-12 13:42:16 -0400

    adds navbar

--- src/components/JumbotronComponent.js
+++ src/components/JumbotronComponent.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Jumbotron = () => {
     return (
         <div className="jumbotron">
-            <h1>To-Do 30</h1><br />
             <p><a href="https://github.com/ejw773/to-do-30" target="_blank" rel="noreferrer">"To-Do 30"</a> is a challenge to create 30 different to-do apps, each within a one-day period.</p>
             <p>My collection of to-do apps is below.</p>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Jumbotron from './JumbotronComponent';
 import CardContainer from './CardContainerComponent';
+import Header from './HeaderComponent';
 
 const Main = () => {
     return (
         <div>
+            <Header />
             <Jumbotron />
             <CardContainer />
         </div>
