5ad32cb967175f90b8da6bea4dd8dfd12884217b
Elijah Wilcott <ejw773@gmail.com>
2021-08-13 16:59:35 -0400

    adjusts header and jumbotron

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { Nav } from 'react-bootstrap';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckDouble } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
         <Navbar bg='dark' variant='dark' expand='sm'>
         <Container>
-            <Navbar.Brand>To-Do 30</Navbar.Brand>
+            <Navbar.Brand><FontAwesomeIcon icon={faCheckDouble} /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
                 <Nav>
                     <Nav.Link href="https://github.com/ejw773/to-do-30" target="_blank" rel="noreferrer">About</Nav.Link>
-                    <Nav.Link href="https://github.com/ejw773/to-do-30/tree/04" target="_blank" rel="noreferrer">Code</Nav.Link>
                     <Nav.Link href="https://elijahwilcott.com/">Portfolio</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
