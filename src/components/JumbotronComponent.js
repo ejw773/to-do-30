7f6156eb9615568d87471a6cbd8c11645f5e346d
Elijah Wilcott <ejw773@gmail.com>
2021-08-13 16:50:19 -0400

    adds fontawesome

--- src/components/JumbotronComponent.js
+++ src/components/JumbotronComponent.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Jumbotron = () => {
     return (
         <div className="jumbotron">
+            <h1 className="display-4">To-Do 30 Challenge</h1>
+            <hr className="my-4" />
             <p><a href="https://github.com/ejw773/to-do-30" target="_blank" rel="noreferrer">"To-Do 30"</a> is a challenge to create 30 different to-do apps, each within a one-day period.</p>
             <p>My collection of to-do apps is below.</p>
         </div>
