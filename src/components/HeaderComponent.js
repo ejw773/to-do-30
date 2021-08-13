0621ca071d8bb8361180125b69fac2156d2cb915
Elijah Wilcott <ejw773@gmail.com>
2021-08-12 20:01:47 -0400

    updates navbar links

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -12,8 +12,9 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="https://github.com/ejw773/to-do-30" target="_blank">About</Nav.Link>
-                    <Nav.Link href="https://elijahwilcott.com/" target="_blank">Portfolio</Nav.Link>
+                    <Nav.Link href="https://github.com/ejw773/to-do-30" target="_blank" rel="noreferrer">About</Nav.Link>
+                    <Nav.Link href="https://github.com/ejw773/to-do-30/tree/04" target="_blank" rel="noreferrer">Code</Nav.Link>
+                    <Nav.Link href="https://elijahwilcott.com/">Portfolio</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
