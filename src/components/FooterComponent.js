cc9c8b9ea4574d8c710503e7b97cc1ca6ac5bfa5
Elijah Wilcott <ejw773@gmail.com>
2021-08-18 11:39:51 -0400

    adds header and footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { classes } from 'istanbul-lib-coverage';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+    footerBar: {
+        top: 'auto',
+        bottom: 0
+    }
+}))
 
 const Footer = () => {
+    const classes = useStyles();
     return (
-        <div></div>
+        <AppBar position='fixed' className={classes.footerBar}>
+            <Toolbar>
+                <Typography style={{align: 'center'}}>Footer</Typography>
+            </Toolbar>
+        </AppBar>
     )
 }
 
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <AppBar>
+            <Toolbar>
+                <Typography variant='h6'>To-Do 30</Typography>
+            </Toolbar>
+        </AppBar>
     )
 }
 
