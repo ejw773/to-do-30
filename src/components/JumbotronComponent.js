e54ee74873e10cb0e6dbedf5a95c4041f883950f
Elijah Wilcott <ejw773@gmail.com>
2021-09-08 09:41:23 -0400

    adjusts video sizing

--- src/components/JumbotronComponent.js
+++ src/components/JumbotronComponent.js
@@ -8,8 +8,12 @@ const Jumbotron = () => {
             <h1 className="display-6">Elijah Wilcott</h1>
             <hr className='my-4' />
             <p>The "To Do App" is the classic exercise in learning web development. This challenge is to create and deploy 30 separate To Do apps, with only one day to create each one.</p>
-            <div class="embed-responsive embed-responsive-16by9">
-              <iframe title='intro-video' className="embed-responsive-item" src="https://www.youtube.com/embed/rQr4AVyOf5o" allowfullscreen></iframe>
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-sm-8">
+                    <div className="ratio ratio-16x9">
+                    <iframe title='intro-video' src="https://www.youtube.com/embed/rQr4AVyOf5o" allowfullscreen></iframe>
+                    </div>
+                </div>
             </div>
         </div>
     )
