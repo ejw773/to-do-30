796da46b54b9b6c53f2790ecf70157437c23a79d
Elijah Wilcott <ejw773@gmail.com>
2021-09-13 22:13:52 -0400

    finished

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ const Main = () => {
             <Header />
             <AddTask />
             <TaskList />
-            <Footer />
+            {/* <Footer /> */}
         </React.Fragment>
     )
 }
