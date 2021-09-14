80556fb8a70c5e3dc824d0436defb483380edbc7
Elijah Wilcott <ejw773@gmail.com>
2021-09-13 20:12:35 -0400

    adds Main component to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/MainComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main />
         <a
           className="App-link"
           href="https://reactjs.org"
