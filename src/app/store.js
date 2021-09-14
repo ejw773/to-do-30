7dcccb0affb5d92281ed4771d8a1f9ed1522f490
Elijah Wilcott <ejw773@gmail.com>
2021-09-13 21:20:45 -0400

    gets redux working

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import taskListReducer from '../features/taskList/taskListSlice';
 
-export const store = configureStore({
-  reducer: {},
+export default configureStore({
+  reducer: {
+    tasks: taskListReducer
+  },
 })
