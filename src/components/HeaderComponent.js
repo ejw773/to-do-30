b1582ef607b318311ae44eafe739a435f098f0bf
Elijah Wilcott <ejw773@gmail.com>
2021-09-16 11:02:59 -0400

    updates readme

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,7 +3,7 @@ import { Navbar } from  'react-bootstrap';
 
 const Header = () => {
     return (
-        <Navbar>
+        <Navbar bg='light' variant='light'>
             <Navbar.Brand>#ToDo30</Navbar.Brand>
         </Navbar>
     )
